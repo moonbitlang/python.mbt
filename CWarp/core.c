f9467f79c8afe0a13a5e1b678c5146e1ab873e66
Kaida-Amethyst <ziyue.liu.vm@gmail.com>
2025-02-12 17:09:50 +0800

    add matplotlib

--- CWarp/core.c
+++ CWarp/core.c
@@ -1,7 +1,13 @@
 #include <Python.h>
 
+#define NULL 0
+
 void print_pyobject(PyObject *obj) { PyObject_Print(obj, stdout, 0); }
 
 void py_incref(PyObject *obj) { Py_INCREF(obj); }
 
 void py_decref(PyObject *obj) { Py_DECREF(obj); }
+
+int Moonbit_PyObjectRef_is_null(PyObject *obj) { return obj == NULL; }
+
+int Moonbit_PyFunction_Check(PyObject *obj) { PyFunction_Check(obj); }
