4f0fad4ec6b9c4871af64340fc469573fd932172
Kaida-Amethyst <ziyue.liu.vm@gmail.com>
2025-05-30 15:21:03 +0800

    move to build.js

--- env.sh
+++
@@ -1,5 +0,0 @@
-export PY_VERSION="$(python3 -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')")"
-export CC=gcc # Or any C Compiler You want
-export CC_FLAGS="-I$(python3-config --prefix)/include/python$PY_VERSION -dynamic -DNDEBUG"
-export CC_LINK_FLAGS="$(python3-config --ldflags) -lpython$PY_VERSION"
-export C_INCLUDE_PATH="$(python3-config --prefix)/include/python$PY_VERSION":$C_INCLUDE_PATH
